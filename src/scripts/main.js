ad1967b0f2951764ee104abe5dce2545063e8d20
Polina Dabizha <polinkadabi29@gmail.com>
2024-12-08 00:19:40 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const tree = document.querySelectorAll('.tree li');
+
+function wrapper(item) {
+  const textNode = item.firstChild.textContent.trim();
+
+  if (textNode) {
+    const span = document.createElement('span');
+
+    span.textContent = textNode;
+
+    item.firstChild.replaceWith(span);
+
+    span.addEventListener('click', (e) => {
+      e.stopPropagation();
+      hideItems(span);
+    });
+  }
+}
+
+tree.forEach((item) => {
+  const hasChildren = item.querySelector('ul');
+
+  if (hasChildren) {
+    wrapper(item);
+  }
+});
+
+function hideItems(item) {
+  const nextItem = item.nextElementSibling;
+
+  nextItem.style.display = nextItem.style.display === 'none' ? '' : 'none';
+}
